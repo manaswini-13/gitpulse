0a83637a1bbe7bfd93c111836d1d085bbbd01ed0
manaswini-13 <manaswiniraokommaraju@gmail.com>
2026-07-08 00:42:47 +0530

    Remove eslint ignoreDuringBuilds configuration
    Removed eslint configuration to ignore linting during builds.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  /* We add this to bypass any strict linting/formatting checks during production compilation */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   }
